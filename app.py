8320fa36b970abba51ab68a01be4ddf34160d0f2
CodeWithEhtisham <shamikhan0347@gmail.com>
2021-11-07 23:59:13 +0500

    version 1 complete

--- app.py
+++ app.py
@@ -404,4 +404,4 @@ def login():
 
 if __name__ == "__main__":
     # app.run(host="127.0.0.1",threaded=True)
-    app.run(host="0.0.0.0",port=4000,threaded=True,debug=True) # home desktop
+    app.run(host="0.0.0.0",port=8000,threaded=True,debug=True) # home desktop
